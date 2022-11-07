0adc6db8c824b4020aece26d8af6ca9ace5f644c
Riya Sun <riya.sunn@gmail.com>
2022-11-07 23:36:52 +1300

    create apollo/client & gql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import{ ApolloClient, InMenoryCache, ApolloProvider } from " @apollo/client";
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -8,20 +9,26 @@ import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
+const client = new ApolloClient({
+  uri:"https://crwn-clothing.com/",
+  cache: new InMenoryCache(),
+});
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
